e40a946c594251ac3b9398ae24e8cde9f6939798
Rafael Medeiros <32855003+Rafamedeiros0@users.noreply.github.com>
2020-05-02 01:16:58 -0300

    create moddels on Product, install requireDir and create a example Product

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const requireDir = require('require-dir')
 
 // Iniciando o App
 const app = express();
@@ -10,11 +11,22 @@ mongoose.connect(
   'mongodb://localhost:27017/nodeapi', 
   {useNewUrlParser: true}
 );
+// caso eu tenha muito models, ele pega todos os que estiverem dentro da pasta, sem precisar fazer um require para cada
+requireDir('./src/models')
+
+const Product = mongoose.model('Product')
 
 
-// Primeira rota
+
+// Primeira rota-
 app.get('/', (req, res) => {
-  res.send('Hello Rafael')
+  Product.create({ 
+    title: 'React Native',
+    description: 'Build native apps with React',
+    url: 'http://github.com/facebook/react-native'
+  })
+
+  return res.send('Hello Rafael')
 })
 
 app.listen(3001);
