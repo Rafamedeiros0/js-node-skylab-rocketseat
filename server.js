0ae9ab7f903c5ec31d5e6cf39df5cb7036d677f2
Rafael Medeiros <32855003+Rafamedeiros0@users.noreply.github.com>
2020-05-02 01:00:11 -0300

    install mongoose and config connection on server.js

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
-const express = require('express');
+const express = require('express')
+const mongoose = require('mongoose')
 
+// Iniciando o App
 const app = express();
 
+// Iniciando o DB
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect(
+  'mongodb://localhost:27017/nodeapi', 
+  {useNewUrlParser: true}
+);
+
+
+// Primeira rota
 app.get('/', (req, res) => {
   res.send('Hello Rafael')
 })
