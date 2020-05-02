4d3af0416d8ee844974e689cf074a74522e0eb5f
Rafael Medeiros <32855003+Rafamedeiros0@users.noreply.github.com>
2020-05-02 01:39:06 -0300

    create route post with method store and config express for using json

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -4,8 +4,14 @@ const Product = mongoose.model('Product')
 
 module.exports = {
   async index(req,res) {
-    const products = await Product.find();
+    const products = await Product.find()
 
     return res.json(products)
+  },
+
+  async store(req, res){
+    const product = await Product.create(req.body)
+
+    return res.json(product)
   }
 }
