85b24faf80c2daa342e70635beb991fe476f45b8
Rafael Medeiros <32855003+Rafamedeiros0@users.noreply.github.com>
2020-05-02 01:28:05 -0300

    create file for routes, create folder for controllers and create ProductController

--- server.js
+++ server.js
@@ -14,19 +14,7 @@ mongoose.connect(
 // caso eu tenha muito models, ele pega todos os que estiverem dentro da pasta, sem precisar fazer um require para cada
 requireDir('./src/models')
 
-const Product = mongoose.model('Product')
-
-
-
-// Primeira rota-
-app.get('/', (req, res) => {
-  Product.create({ 
-    title: 'React Native',
-    description: 'Build native apps with React',
-    url: 'http://github.com/facebook/react-native'
-  })
-
-  return res.send('Hello Rafael')
-})
+// ROtas
+app.use('/api', require('./src/routes'))
 
 app.listen(3001);
